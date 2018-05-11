4ca830e8ab207983fc1508f396568c423f3c8cb4
tao-li06 <litao006@gmail.com>
2018-05-10 17:43:10 -0700

    Remove babel

--- index.js
+++ index.js
@@ -5,18 +5,22 @@ const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
-app.prepare().then(() => {
-  const server = express();
+app.prepare()
+  .then(() => {
+    const server = express();
 
-  server.get('/r/:subreddit', (req, res) => {
-    return app.render(req, res, '/b', {
-      ...req.query,
-      subreddit: req.params.subreddit
-    })
-  });
+    server.get('/r/:subreddit', (req, res) => {
+      return app.render(req, res, '/b', {
+        ...req.query,
+        subreddit: req.params.subreddit
+      })
+    });
 
-  server.get('*', (req, res) => {
-    handle(req, res)
+    server.get('*', (req, res) => {
+      handle(req, res)
+    });
+
+    //initializeHandlers(server);
+
+    server.listen(80);
   });
-  server.listen(3000);
-});
