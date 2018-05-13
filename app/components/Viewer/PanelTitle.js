90a4189c202405592c100f1b566e97133631bdd4
xiangyin701 <xiangyin701@gmail.com>
2018-05-12 18:58:44 -0700

    Added api to display full name in the title

---
+++ app/components/Viewer/PanelTitle.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import { getProteinInfo } from '../../service/uniprot';
+
+class ProteinTitle extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { loaded: false };
+    }
+
+    async componentWillMount() {
+      return await this.updateInfo(this.props);
+    }
+
+    async componentWillReceiveProps(props) {
+      return await this.updateInfo(props);
+    }
+
+    async updateInfo(props) {
+      this.setState({ loaded: false });
+      const { id } = props;
+      const info = await getProteinInfo(id);
+      this.setState({
+        loaded: true,
+        identifier: info.id,
+        fullName: info.protein.recommendedName.fullName.value
+      });
+    }
+
+    render() {
+      const { id } = this.props;
+      const { loaded, identifier, fullName } = this.state;
+      if (!loaded) {
+        return (
+          <h4>{id}</h4>
+        );
+      }
+      return (
+        <h4>{id} ( {`${identifier} ${fullName}`} )</h4>
+      );
+
+    }
+}
+
+
+export default ProteinTitle;
