86bd84a4283bbd9dc755097dd9767e6dc7f4d434
xiangyin701 <xiangyin701@gmail.com>
2018-11-13 21:33:51 -0800

    Bug fix

--- app/config/config.js
+++ app/config/config.js
@@ -1,12 +1,12 @@
 const globalConfig = {
   development: {
     endpoints: {
-      darts: 'http://35.233.139.236',
+      darts: 'http://www.darts-sci.com',
     }
   },
   production: {
     endpoints: {
-      darts: 'http://35.233.139.236',
+      darts: 'http://www.darts-sci.com',
     }
   }
 }
